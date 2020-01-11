7822e6d2b62330dba7b95500aef228b93ef3b111
igor silva <igoraparecido59@outlook.com>
2020-01-11 14:48:11 -0300

    Add style to components

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Container,Label } from './styles';
+
 export default function Card() {
     return (
-        <div></div>
+      <Container>
+        <header>
+          <Label color="#7159C1"/>
+        </header>
+        <p>Fazer a migração completa de servidor</p>
+        <img src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="avatar"/>
+      </Container>
     )
 }
