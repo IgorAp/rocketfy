fc0b3ea569813f2d01203072c62ddd82b628f8f6
igor silva <igoraparecido59@outlook.com>
2020-01-11 13:54:03 -0300

    Add base components

---
+++ src/components/List/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function List() {
+    return (
+        <Container>
+            <header>
+                <h2>Tarefas</h2>
+                <button type="button">
+                    +
+                </button>
+            </header>
+
+        </Container>
+    )
+}
